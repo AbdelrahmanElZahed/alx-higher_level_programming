7b7ab07845b4c160680fe1258a12d74e6a61aecf
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-09-11 02:35:45 +0300

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def print_tebahpla():
-    print("".format(
-        *(
-            chr(122 - i) if i % 2 == 0 else chr(90 - (i - 1))
-            for i in range(26)
-        )
-    ), end="")
+    result = ''.join(
+        chr(122 - i) if i % 2 == 0 else chr(90 - i)
+        for i in range(25, -1, -1)
+    )
+    print("{}".format(result), end="")
 
 print_tebahpla()
