e739f7e16f67a40dc2db2c76770af2e75a8835da
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-10-02 05:18:03 +0300

    Printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -39,5 +39,6 @@ class Square:
     def my_print(self):
         """Prints this square."""
         for i in range(self.size):
-            print("#", end="\n" if j is self.size - 1 and i !=j else "")
+            for j in range(self.size):
+                print("#", end="\n" if j is self.size - 1 and i != j else "")
         print()
