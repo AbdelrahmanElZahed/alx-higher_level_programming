91fa5140f955e238dd6748fedddd45a5c15a0ffc
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-10-02 04:51:22 +0300

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""square module."""
+
+class Square:
+    """Define a square."""
+    
+    def __init__(self, size):
+        """Initialize the size of the square."""
+        self.__size = size  # Private instance attribute
+
