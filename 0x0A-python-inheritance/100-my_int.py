61f428ae5d39dbf144093e7a0d40271b2e181ab6
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-10-15 06:54:09 +0300

    12. My integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+'''Module that definrs myint inherts drom int.'''
+
+
 class MyInt(int):
-    """MyInt is a rebel integer class."""
-
+    '''MyInt is a rebel integer class.'''
     def __eq__(self, other):
-        """Invert equality comparison."""
+        '''Invert equality comparison.'''
         return super().__ne__(other)
-
     def __ne__(self, other):
-        """Invert inequality comparison."""
+        '''Invert inequality comparison.'''
         return super().__eq__(other)
 
