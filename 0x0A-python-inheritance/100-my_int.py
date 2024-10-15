e3ec12a451b675fa2d4c33ebdedc2f098568d060
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-10-15 06:02:06 +0300

    12. My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class MyInt(int):
+    """MyInt is a rebel integer class."""
+
+    def __eq__(self, other):
+        """Invert equality comparison."""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Invert inequality comparison."""
+        return super().__eq__(other)
+
